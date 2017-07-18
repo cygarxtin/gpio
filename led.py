8cbb1e53da4904857c140f65a7bc4a2ccfd613c2
Assigeh Delanyo <delanyoassigeh@gmail.com>
2017-07-18 10:32:00 +0000

    enable led

---
+++ led.py
@@ -0,0 +1,20 @@
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+def dot():
+	led.on()
+	sleep(1)
+	led.off()
+	sleep(1)
+
+def dash():
+
+
+
+while True:
+	led.on()
+	sleep(1)
+	led.off()
+	sleep(1)
