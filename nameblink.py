550fe48190864c32de17b3054cfdbfcd66ca53c5
Assigeh Delanyo <delanyoassigeh@gmail.com>
2017-07-18 14:49:20 +0000

    IoT

---
+++ nameblink.py
@@ -0,0 +1,42 @@
+from gpiozero import LED, Button
+from signal import pause
+from time import sleep
+
+led =LED(17)
+button = Button
+
+button.when_pressed = led.on
+button.when_pressed = led.off
+
+def dot():
+	led.on()
+	sleep(0.5)
+	led.off()
+	sleep(0.5)
+
+def dash():
+	led.on()
+	sleep(0.5)
+	led.off()
+	sleep(1)
+
+def c():
+	dot()
+def y():
+	dash()
+	dot()
+	dash()
+	dot()
+def g():
+	dot()
+	dash()
+
+def say_my_name():
+	c()
+	y()
+	g()
+
+
+button.when_pressed = say_my_name
+
+pause()
